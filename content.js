1069f4eade4b889df10e460ca67b12c2cd9e57bc
abraham <4braham@gmail.com>
2011-05-24 23:22:17 -0700

    cleaned up code

--- content.js
+++ content.js
@@ -1,43 +1,29 @@
 console.log('Loading Timely');
-var timelyChromeReset = (new Date()).getTime();
+var type = 'newTwitter',
+    formSelectors = { newTwitter: 'div.tweet-box', intent: '#update-form' },
+    submitSelectors = { newTwitter: 'a.tweet-button', intent: 'input.submit' },
+    timelyChromeReset = (new Date()).getTime();
 
 if (window.location.pathname && window.location.pathname === '/intent/tweet') {
-  $('#update-form')
-    .addClass('timely')
-    .find('input.submit')
-    .before("<a href='#' class='tweet-button timely-button button disabled'><img width='13px' height='13px' src='" + chrome.extension.getURL('clock.png') + "' title='Schedule with Timely' /></a>");
-
-  $('a.timely-button').click(openPopup);
-  $('#status').keyup(  function() {
-        var $this = $(this),
-            $button = $this.parents('#update-form').find('a.timely-button');
-
-        if ($this.val().length > 0) {
-          $button.removeClass('disabled');
-        } else {
-          $button.addClass('disabled');
-        }
-      });
+  type = 'intent';
+  attatchButton()
 } else {
-  $('.twitter-anywhere-tweet-box-editor').live('focus', attachDefaultButton);
+  $('.twitter-anywhere-tweet-box-editor').live('focus', attatchButton);
 }
 
-function attachDefaultButton() {
-  // Find DM fields and mark to avoid.
+function attatchButton() {
   $('a.tweet-button:contains("Send")').parents('div.tweet-box').addClass('no-timely');
-  // Attach button to form.
-  $('div.tweet-box:not(.timely):not(.no-timely)')
+  $(formSelectors[type] + ':not(.timely):not(.no-timely)')
     .addClass('timely')
-    .find('a.tweet-button')
+    .find(submitSelectors[type])
     .before("<a href='#' class='tweet-button timely-button button disabled'><img width='13px' height='13px' src='" + chrome.extension.getURL('clock.png') + "' title='Schedule with Timely' /></a>");
-  $('.twitter-anywhere-tweet-box-editor').keyup(triggerKeyup);
   $('a.timely-button').click(openPopup);
+  $('textarea').keyup(triggerKeyup);
 }
 
 function triggerKeyup() {
   var $this = $(this),
-      $button = $this.parents('div.tweet-box').find('a.timely-button');
-  
+      $button = $this.parents(formSelectors[type]).find('a.timely-button');
   if ($this.val().length > 0) {
     $button.removeClass('disabled');
   } else {
@@ -52,15 +38,14 @@ function openPopup() {
   }
 
   var $this = $(this),
-      $textarea = $this.parents('div.tweet-box').find('textarea').length ? $this.parents('div.tweet-box').find('textarea') : $this.parents('#update-form').find('textarea'),
+      $textarea = $this.parents(formSelectors[type]).find('textarea'),
       width = 500,
       height = 225,
       left = Math.round((screen.width - width) / 2),
       top = Math.round((screen.height - height) / 2);
   window.ft = window.open('http://timely.flowtown.com/bookmarklet?s=' + encodeURIComponent($textarea.val()), '', 'left=' + left + ',top=' + (top > 0 ? top : 0) + ',width=' + width + ',height=' + height + ',personalbar=0,toolbar=0,scrollbars=0,resizable=1');
-      
-  $this.addClass('disabled');
+  
+  $this.addClass('disabled').next().addClass('disabled');
   $textarea.val('');
-  $textarea.trigger('keyup');
   timelyChromeReset = (new Date()).getTime();
 }
